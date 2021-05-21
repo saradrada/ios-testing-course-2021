de9f211a9497bd48e22f65e1446cbdc7d6db311a
saradrada <sara.ortizd@perficient.com>
2021-05-21 10:23:37 -0500

    Initial Commit

---
+++ Shared/photoappApp.swift
@@ -0,0 +1,17 @@
+//
+//  photoappApp.swift
+//  Shared
+//
+//  Created by Sara Ortiz Drada on 21/05/21.
+//
+
+import SwiftUI
+
+@main
+struct photoappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
